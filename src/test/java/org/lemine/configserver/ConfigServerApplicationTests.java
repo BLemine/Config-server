f987f473af36d4c21a889427dca72ac319948c97
BLemine <baillahiamine@gmail.com>
2025-04-27 21:35:52 +0100

    add config-server with postgresql database

---
+++ src/test/java/org/lemine/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemine.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
